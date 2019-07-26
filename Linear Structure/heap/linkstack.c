27319b2e537a7d77afabfcdd1f5c0d7405a6aa1d
Closedboy <50324484+Closedboy@users.noreply.github.com>
2019-07-26 20:28:24 +0800

    Create linkstack.c

---
+++ Linear Structure/heap/linkstack.c
@@ -0,0 +1,47 @@
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkstack.h"
+
+linkstack CreateStack()
+{
+    linkstack S;
+    S = (linkstack)malloc(sizeof(struct lSNode));
+    S->Next = NULL;
+    return S;
+}
+
+int IsEmpty(linkstack S)
+{
+    return ( S->Next == NULL );
+}
+
+void Push(linkstack S, ELementType item)
+{
+    linkstack temp;
+    temp = (linkstack)malloc(sizeof(struct lSNode));
+    temp->Data = item;
+    temp->Next = S->Next;
+    S->Next = temp;
+}
+
+ELementType Pop(linkstack S)
+{
+    ELementType TopItem;
+    if(IsEmpty(S))
+    {
+        printf("Stack is empty!\n");
+        return ERROR;
+    }
+    else
+    {
+        linkstack temp;
+        temp = S->Next;
+        S->Next = temp->Next;
+        TopItem = temp->Data;
+        free(temp);
+        temp = NULL;
+        return TopItem;
+    }
+}
+```
