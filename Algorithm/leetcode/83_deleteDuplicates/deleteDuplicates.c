f597b75dbbda240e0e350821fff6ef1b8409b938
Closedboy <50324484+Closedboy@users.noreply.github.com>
2019-08-03 09:53:17 +0800

    Create deleteDuplicates.c

---
+++ Algorithm/leetcode/83_deleteDuplicates/deleteDuplicates.c
@@ -0,0 +1,14 @@
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    struct ListNode *uniq = head, *p = head;
+    while(p)
+    {
+        if( p->val != uniq->val )
+        {
+            uniq->next = p;
+            uniq = p;
+        }
+        p = p->next;
+        uniq->next = NULL;
+    }
+    return head;
+}
