60c318753ccc68b3fdf555628bace3db082118e6
Closedboy <50324484+Closedboy@users.noreply.github.com>
2019-08-03 11:40:11 +0800

    Update deleteDuplicatesRc.c

--- Algorithm/leetcode/83_deleteDuplicates/deleteDuplicatesRc.c
+++ Algorithm/leetcode/83_deleteDuplicates/deleteDuplicatesRc.c
@@ -3,7 +3,7 @@ struct ListNode* deleteDuplicatesRc(struct ListNode* head){
         return head;
     else
     {
-        head->next = deleteDuplicates(head->next);
+        head->next = deleteDuplicatesRc(head->next);
         if( head->val == head->next->val )
         {
             struct ListNode *temp = head;
