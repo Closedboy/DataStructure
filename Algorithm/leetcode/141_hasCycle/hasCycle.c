b830eaf7d19ce43925451fe6e766a8e92a02f360
Closedboy <50324484+Closedboy@users.noreply.github.com>
2019-08-03 14:39:40 +0800

    Update hasCycle.c

--- Algorithm/leetcode/141_hasCycle/hasCycle.c
+++ Algorithm/leetcode/141_hasCycle/hasCycle.c
@@ -2,14 +2,14 @@ bool hasCycle(struct ListNode *head) {
     struct ListNode *slow, *fast;
     if(!head)
         return false;
-    slow = head;
-    fast = head->next;
+    slow = head; /* 慢指针 */
+    fast = head->next; /* 快指针 */
     while(fast && fast->next)
     {
         if( slow == fast )
             return true;
-        slow = slow->next;
-        fast = fast->next->next;
+        slow = slow->next; /* 慢指针每次走一步 */
+        fast = fast->next->next; /* 快指针一次走两步 */
     }
     return false;
 }
